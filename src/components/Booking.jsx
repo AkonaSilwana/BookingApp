1680d3df26ce9fd0001b1c3a6b9e85bb68ec8095
AkonaSilwana <109544433+AkonaSilwana@users.noreply.github.com>
2023-10-31 00:49:14 +0200

    added confirmation model and adding data array

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,18 +1,41 @@
 import React, {useState} from "react";
 import { useParams } from 'react-router-dom';
+let data = [
+    {
+      "date": "2016-05-18",
+      "availability": [9, 10, 11, 12, 13, 14, 17]
+    },
+    {
+      "date": "2016-05-19",
+      "availability": [9, 10, 11, 12, 13, 14, 15, 16, 17]
+    },
+    {
+      "date": "2016-05-20",
+      "availability": [9, 10, 14, 15, 16, 17]
+    },
+    {
+      "date": "2016-05-21",
+      "availability": [9, 10, 11, 12, 13]
+    },
+    {
+      "date": "2016-05-23",
+      "availability": [13, 14, 15, 16]
+    },
+    {
+      "date": "2016-05-24",
+      "availability": [11, 12, 15, 16, 17]
+    }
+]
 
 const Booking = () => {
-
-    const [sliderValue, setSliderValue] = useState(1);
-    const [isChecked, setIsChecked] = useState(false);
+    const [selectedSlots, setSelectedSlots] = useState([]);
+    const [sliderValue, setSliderValue] = useState(2);
+    const [showConfirmationPopup, setShowConfirmationPopup] = useState(false);
+    const [showSuccessPopup, setShowSuccessPopup] = useState(false);
 
     const handleSliderChange = (event) => {
       const newValue = parseInt(event.target.value, 6);
       setSliderValue(newValue);
-    };
-
-    const handleInputChange = (e) => {
-        setIsChecked(e.target.checked);
     };
     const { selectedDate } = useParams();
     const currentDate = new Date(selectedDate);
@@ -22,16 +45,78 @@ const Booking = () => {
         timeSlots: []
     }
 
-    for(let i = 9; i <= 16; i++) {
-        times.timeSlots.push(`${i}:00-${i+1}:00`)
-    }
-    
     for(let i = 0; i < 6; i++) {
         const nextDate = new Date(currentDate);
         nextDate.setDate(currentDate.getDate() + i);
         times.days.push(nextDate.toDateString());
     }
+    const checkSlotAvailability = (timeSlot, jobLength, availability) => {
+        const bufferHours = 1; 
+        const slotStartTime = new Date(selectedDate + 'T' + timeSlot + ':00:00').getTime();
+        const slotEndTime = slotStartTime + jobLength * 60 * 60 * 1000;
 
+        for (const availableHour of availability) {
+            const availableStartTime = new Date(selectedDate + 'T' + availableHour + ':00:00').getTime();
+            const availableEndTime = availableStartTime + (jobLength + 2 * bufferHours) * 60 * 60 * 1000;
+
+            if (slotStartTime >= availableStartTime && slotEndTime <= availableEndTime) {
+                return 'available';
+            }
+        }
+
+        return 'full';
+    }
+     const handleSlotClick = (date, slot) => {
+    if (checkSlotAvailability(slot, sliderValue, date, data)) {
+      if (!selectedSlots.includes(`${date}-${slot}`)) {
+        setSelectedSlots([...selectedSlots, `${date}-${slot}`]);
+        console.log(`Slot ${slot} on ${date} is available.`);
+      } else {
+        setSelectedSlots(selectedSlots.filter((selected) => selected !== `${date}-${slot}`));
+        console.log(`Slot ${slot} on ${date} is unselected.`);
+      }
+    } else {
+      console.log(`Slot ${slot} on ${date} is unavailable.`);
+    }
+  };
+    
+    const handleConfirmBooking = () => {
+        setShowConfirmationPopup(true);
+    };
+    
+    const handleConfirmSubmit = () => {
+        const updatedData = data.map((item) => {
+            return {
+              ...item,
+              availability: item.availability.filter((slot) => !selectedSlots.includes(`${item.date}-${slot}`)),
+            };
+          });
+      
+          data = updatedData;
+          setSelectedSlots([]);
+          setShowConfirmationPopup(false);
+          setShowSuccessPopup(true);
+    };
+
+    const handleCancelBooking = () => {
+        setShowConfirmationPopup(false);
+        setSelectedSlots([]); 
+      };
+    
+      const getAvailabilityForDate = (date) => {
+        const dateData = data.find((item) => item.date === date);
+        return dateData ? dateData.availability : [];
+      };
+      const isSlotFullyBooked = (date, slot) => {
+        const dateData = data.find((item) => item.date === date);
+        return dateData ? !dateData.availability.includes(slot) : false;
+      };
+    // const formatDate = (inputDate) => {
+    //     const options = { weekday:'long', month:'long', day:'numeric'};
+    //     const formattedDate = new Date(inputDate).toLocaleDateString(undefined, options);
+    //     return formattedDate
+    // }
+    
     return(
         <div id="booking">
             <h1 className="title">Booking Calendar</h1>
@@ -60,38 +145,75 @@ const Booking = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {times.timeSlots.map((slot, index) => (
-                            <tr key={index}>
-                                <td className="time-slot">{slot}</td>
-                                <td>
-                                    <label htmlFor="pickedDay" className="full">Full</label>
-                                    <input type="checkbox" name="pickedDay" id="pickedDay" className="full" />
-                                </td>
-                                <td>
-                                    <label htmlFor="pickedDay2" className={isChecked ? 'checked' : ''} >Available</label>
-                                    <input type="checkbox" name="pickedDay2" id="pickedDay2" checked={isChecked} onChange={handleInputChange}/>
-                                </td>
-                                <td>
-                                    <label htmlFor="pickedDay">Available</label>
-                                    <input type="checkbox" name="pick_time" id="pickedDay" />
-                                </td>
-                                <td>
-                                    <label htmlFor="pickedDay">Available</label>
-                                    <input type="checkbox" name="pick_time" id="pickedDay" />
-                                </td>
-                                <td>
-                                    <label htmlFor="pickedDay">Available</label>
-                                    <input type="checkbox" name="pick_time" id="pickedDay" />
-                                </td>
-                                <td>
-                                    <label htmlFor="pickedDay">Available</label>
-                                    <input type="checkbox" name="pick_time" id="pickedDay" />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            {[...Array(9).keys()].map((hour) => (
+              <tr key={hour + 9}>
+                <th>{hour + 9}:00 - {hour + 10}:00</th>
+                {data.map((item) => {
+                  const date = item.date;
+                  const slot = hour + 9;
+                  const isSelected = selectedSlots.includes(`${date}-${slot}`);
+                  const isFullyBooked = isSlotFullyBooked(date, slot);
+
+                  let cellClass = '';
+                  if (isSelected) {
+                    cellClass = 'selected';
+                  } else if (isFullyBooked) {
+                    cellClass = 'full';
+                  } else if (getAvailabilityForDate(date).includes(slot)) {
+                    cellClass = 'available';
+                  } else {
+                    cellClass = 'empty';
+                  }
+
+                  return (
+                    <td
+                      key={`${date}-${slot}`}
+                      className={cellClass}
+                      onClick={() => {
+                        if (cellClass === 'available') {
+                          handleSlotClick(date, slot);
+                        }
+                      }}
+                      disabled
+                    >
+                      {isSelected ? 'Selected' : isFullyBooked ? 'Fully Booked' : (cellClass === 'available' ? 'Available' : '')}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
             </div>
+            {selectedSlots.length > 0 && (
+                <button onClick={handleConfirmBooking} className="submit-selected shadow">Submit {selectedSlots.length} Selected Slots</button>
+            )}
+            {showConfirmationPopup && (
+                <div className="popup-container">
+                    <div className="popup confirmation shadow">
+                        <h4>Are you sure you want to book these slots?</h4>
+                        <p>You have selected the following slots:</p>
+                        <ul>
+                            {selectedSlots.map((slot) => (
+                            <li key={slot}>{slot}</li>
+                            ))}
+                        </ul>
+                        <div className="action-btns">
+                            <button onClick={handleCancelBooking}>Cancel</button>
+                            <button onClick={handleConfirmSubmit} className="main-btn">Yes, Book</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {showSuccessPopup && (
+                <div className="popup-container">
+                    <div className="popup success-popup">
+                        <h4>Successfully booked slots!</h4>
+                        <button onClick={() => setShowSuccessPopup(false)}>Close</button>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
